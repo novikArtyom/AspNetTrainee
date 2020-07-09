3534e5a35e2016336f028b156a02f87e530ddef2
novikArtyom <novik.artsem2000@gmail.com>
2020-07-09 15:38:04 +0300

    add DbInitializer and view for it all

---
+++ AspNetTrainee/Models/DbInitializer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace AspNetTrainee.Models
+{
+    public class DbInitializer: DropCreateDatabaseAlways<PhoneContext>
+    {
+        protected override void Seed(PhoneContext db)
+        {
+            db.Phones.Add(new Phone { Name = "iPhone 11 Pro Max", Producer = "Apple", Price = 999 });
+            db.Phones.Add(new Phone { Name = "Samsung Galaxy S20 Ultra", Producer = "Samsung", Price = 1199 });
+            db.Phones.Add(new Phone { Name = "Huawei P40 Pro", Producer = "Huwaei", Price = 1250 });
+
+            base.Seed(db);
+        }
+    }
+}
