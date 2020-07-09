159fe7482495adaf40857cf7dfcd670f280a730b
novikArtyom <novik.artsem2000@gmail.com>
2020-07-09 14:35:32 +0300

    added EF files, created models and DbContext

---
+++ AspNetTrainee/Models/Phone.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AspNetTrainee.Models
+{
+    public class Phone
+    {
+        private int Id { get; set; }
+
+        private string Name { get; set; }
+
+        private string Producer { get; set; }
+
+        private double Price { get; set; }
+
+    }
+}
---
+++ AspNetTrainee/Models/Purchase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AspNetTrainee.Models
+{
+    public class Purchase
+    {
+
+        private int PurchaseId { get; set; }
+
+        private string BuyerName { get; set; }
+
+        private string BuyerPhone { get; set; }
+
+        private string BuyerMail { get; set; }
+
+        public int PhoneId { get; set; }
+    }
+}
